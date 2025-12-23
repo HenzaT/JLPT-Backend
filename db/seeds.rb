f28bd7ac7d6d45fc38e6f55a286b0a3b46898e0e
Henry Thomas <henrykun95@gmail.com>
2025-12-23 10:38:39 +0000

    experimenting with seed file, get jlpt 5 kanji and specific kanji object

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,38 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+require 'net/http'
+require 'addressable/uri'
+
+puts '--- Cleaning Database ---'
+KanjiCharacter.destroy_all
+
+# kanji_api base url
+url = 'https://kanjiapi.dev/v1/kanji'
+
+# method for jlpt urls
+def jlpt_url(level)
+  "https://kanjiapi.dev/v1/kanji/jlpt-#{level}"
+end
+
+jlpt5 = jlpt_url('5')
+jlpt4 = jlpt_url('4')
+jlpt3 = jlpt_url('3')
+jlpt2 = jlpt_url('2')
+jlpt1 = jlpt_url('1')
+
+# array holding all kanji regardless of jlpt level
+all_kanji = []
+
+parsed = URI.parse(jlpt5).read
+characters = JSON.parse(parsed)
+characters.each do |c|
+  all_kanji << c
+end
+
+url2 = "#{url}/#{all_kanji[4]}"
+
+parsed2 = Addressable::URI.parse(url2)
+parsed2.normalize!
+response = Net::HTTP.get(parsed2)
+kanji = JSON.parse(response)
+
+puts kanji
