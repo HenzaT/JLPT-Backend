2ff92844d9f12b288ff777eef02dd4d0c4de4e7b
Henry Thomas <henrykun95@gmail.com>
2025-12-24 10:16:33 +0000

    saved all kanji to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,13 @@ require 'json'
 require 'net/http'
 require 'addressable/uri'
 
-puts '--- Cleaning Database ---'
+puts '-----------------------------'
+puts '----- Cleaning Database -----'
+puts '-----------------------------'
 KanjiCharacter.destroy_all
 
 # kanji_api base url
-url = 'https://kanjiapi.dev/v1/kanji'
+base_url = 'https://kanjiapi.dev/v1/kanji'
 
 # method for jlpt urls
 def jlpt_url(level)
@@ -19,20 +21,49 @@ jlpt3 = jlpt_url('3')
 jlpt2 = jlpt_url('2')
 jlpt1 = jlpt_url('1')
 
+# array holding all jlpt urls
+all_jlpt_url = [jlpt5, jlpt4, jlpt3, jlpt2, jlpt1]
+
 # array holding all kanji regardless of jlpt level
 all_kanji = []
 
-parsed = URI.parse(jlpt5).read
-characters = JSON.parse(parsed)
-characters.each do |c|
-  all_kanji << c
+# parse each jlpt url > append all_kanji array with each individual character
+all_jlpt_url.each do |url|
+  url_json = URI.parse(url).read
+  characters = JSON.parse(url_json)
+  characters.each do |c|
+    # insert kanji into all_kanji array
+    all_kanji << c
+  end
 end
 
-url2 = "#{url}/#{all_kanji[4]}"
+# take each character, pass it in to the base_url and match model columns with api values. Save into db
+all_kanji.each do |kanji_character|
+  get_kanji_url = "#{base_url}/#{kanji_character}"
+  uri = Addressable::URI.parse(get_kanji_url) # use Addressable gem as 'kanji' is a non ascii character
+  uri.normalize!
+  response = Net::HTTP.get(uri)
+  kanji = JSON.parse(response)
 
-parsed2 = Addressable::URI.parse(url2)
-parsed2.normalize!
-response = Net::HTTP.get(parsed2)
-kanji = JSON.parse(response)
+  kun_readings = kanji['kun_readings']
+  on_readings  = kanji['on_readings']
+  meanings     = kanji['meanings']
+  heisig_en    = kanji['heisig_en']
+  jlpt         = kanji['jlpt']
+  kanji        = kanji['kanji']
 
-puts kanji
+  KanjiCharacter.create!(
+    jlpt: jlpt,
+    kanji: kanji,
+    heisig_en: heisig_en,
+    kun_readings: kun_readings,
+    on_readings: on_readings,
+    meanings: meanings
+  )
+
+  puts '----- Kanji Saved -----'
+end
+
+puts '----------------------------------------'
+puts '----- All Kanji Saved to Database! -----'
+puts '----------------------------------------'
